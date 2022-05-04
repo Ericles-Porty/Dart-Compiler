a5d446e9039398158a949b0251fe3c8256190a30
MariaMilena <65188572+MariaMilena@users.noreply.github.com>
2022-05-04 11:36:34 -0300

    Update syntatic.py

--- src/syntatic/syntatic.py
+++ src/syntatic/syntatic.py
@@ -5,10 +5,10 @@ def p_program(p):
     '''program : funcdecl
                 | funcdecl program
                 '''
-    # if (len(p) == 3):
-    #     p[0] = [p[1]] + p[2]
-    # else:
-    #     p[0] = [p[1]]
+    if (len(p) == 3):
+        p[0] = [p[1]] + p[2]
+    else:
+        p[0] = [p[1]]
 
 def p_funcdecl(p):
     '''funcdecl : signature body'''
@@ -32,8 +32,8 @@ def p_sigparams(p):
     #     p[0] = sa.CompoundSigParams(p[1], p[2], p[4])
 
 def p_body(p):
-    ''' body : LCHAV stms RCHAV
-             | LCHAV RCHAV'''
+    ''' body : LKEY stms RKEY
+             | LKEY RKEY'''
     # if (len(p) == 4):
     #     p[0] = sa.BodyConcrete(p[2])
     # else:
@@ -48,9 +48,9 @@ def p_stms(p):
     #     p[0] = sa.CompoundStm(p[1], p[2])
 
 def p_stm(p):
-    ''' stm :  exp PV
+    ''' stm :  exp SEMICOLON
              | WHILE LPAREN exp RPAREN body
-             | RETURN exp PV'''
+             | RETURN exp SEMICOLON'''
     # if (len(p) == 3):
     #     p[0] = sa.StmExp(p[1])
     # elif (p[1] == 'while'):
@@ -61,15 +61,15 @@ def p_stm(p):
     #     print('Gerei None', p[1])
 
 def p_exp_assign(p):
-    ''' exp :    exp IGUAL exp1
+    ''' exp :    exp EQUALS exp1
               | exp1'''
     # if (len(p) == 2):
     #     p[0] = p[1]
     # else:
     #     p[0] = sa.AssignExp(p[1], p[3])
 
-def p_exp1_soma(p):
-    '''exp1 : exp1 SOMA exp2
+def p_exp1_sum(p):
+    '''exp1 : exp1 SUM exp2
          | exp2'''
     # if len(p) == 2:
     #     p[0] = p[1]
@@ -77,8 +77,8 @@ def p_exp1_soma(p):
     #     p[0] = sa.SomaExp(p[1], p[3])
 
 
-def p_exp2_vezes(p):
-   '''exp2 : exp2 VEZES exp3
+def p_exp2_sub(p):
+   '''exp2 : exp2 SUB exp3
            | exp3'''
 #    if len(p) == 2:
 #        p[0] = p[1]
@@ -86,13 +86,29 @@ def p_exp2_vezes(p):
 #        p[0] = sa.MulExp(p[1], p[3])
 
 
-def p_exp3_pot(p):
-    '''exp3 : exp4 POT exp3
+def p_exp3_times(p):
+    '''exp3 : exp4 TIMES exp3
             | exp4'''
     # if len(p) == 2:
     #     p[0] = p[1]
     # else:
     #     p[0] = sa.PotExp(p[1], p[3])
+
+def p_exp4_times(p):
+    '''exp4 : exp5 DIV exp4
+            | exp5'''
+
+def p_exp5_div_part_int(p):
+    '''exp5 : exp6 DIV_PART_INT exp5
+            | exp6'''
+
+def p_exp6_div_rest(p):
+    '''exp6 : exp7 DIV_REST exp6
+            | exp7'''
+
+def p_exp7_div_rest(p):
+    '''exp7 : SUB exp8 
+            | exp8'''
 
 def p_exp4_call(p):
     '''exp4 : call

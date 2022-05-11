b5705a32b38f9f8b654daf6b3c119036dc7a1d8f
MariaMilena <65188572+MariaMilena@users.noreply.github.com>
2022-05-11 12:37:19 -0300

    Update tokens.py
    Adicionando novos tokens

--- src/tokens.py
+++ src/tokens.py
@@ -1,1 +1,19 @@
-tokens = ('ID', 'NUMBER', 'CLASS', 'IS', 'VAR', 'RETURN', 'WHILE',  'IF', 'ELSE',  'TRUE', 'FALSE', 'NULL', 'SUM', 'SUB', 'TIMES', 'DIV', 'DIV_PART_INT', 'DIV_REST', 'EQUALS', 'DIFF', 'GREATER_EQ', 'LESS_EQ', 'GREATER', 'LESS', 'INVERT_EXPR', 'OR', 'AND', 'AND_BIN', 'OR_BIN', 'INVERT_BIN', 'XOR', 'LPAREN', 'RPAREN', 'COMMA' ,'SEMICOLON',  'ASSIGN', 'COMMENT_BLOCK', 'COMMENT_DOCUMENTATION', 'COMMENT_LINE', 'LKEY', 'RKEY', 'INT', 'STRING') 
+reservadas = {
+  'while' : 'WHILE',
+  'for' : 'FOR',
+  'true' : 'TRUE',
+  'false' : 'FALSE',
+  'return' : 'RETURN',
+  # 'int' : 'INT',
+  # 'var' : 'VAR',
+  'class' : 'CLASS',
+  'is' : 'IS',
+  'if' : 'IF',
+  'else' : 'ELSE',
+  'null' : 'NULL',
+  'static' : 'STATIC',
+  'import' : 'IMPORT'
+  # 'string' : 'STRING'
+}
+
+tokens = ['ID', 'NUMBER', 'DOUBLE_QUOTES','SUM', 'SUB', 'TIMES', 'DIV', 'DIV_PART_INT', 'DIV_REST', 'EQUALS', 'DIFF', 'GREATER_EQ', 'LESS_EQ', 'GREATER', 'LESS', 'INVERT_EXPR', 'OR', 'AND', 'AND_BIN', 'OR_BIN', 'INVERT_BIN', 'XOR', 'LPAREN', 'RPAREN', 'COMMA' ,'SEMICOLON',  'ASSIGN', 'COMMENT_BLOCK', 'COMMENT_DOCUMENTATION', 'COMMENT_LINE', 'LKEY', 'RKEY'] + list(reservadas.values())

9f7a61f5a158c412dae5c5cd5f639c70d82dbc91
Ericles-Porty <ericlesdsantos@gmail.com>
2022-05-19 11:16:44 -0300

    UPDATE::SyntaxAbstract

--- src/syntatic/syntatic.py
+++ src/syntatic/syntatic.py
@@ -1,10 +1,11 @@
 import ply.yacc as yacc
 from src.tokens import *
+from src.syntatic.syntax_abstract import *
 
-def p_import(p):
+def p_header(p):
     '''import : program
-                | IMPORT string
-                | IMPORT string program
+                | IMPORT string SEMICOLON
+                | IMPORT string SEMICOLON import
                 '''
 
 def p_program(p):
@@ -13,48 +14,48 @@ def p_program(p):
                 | CLASS ID body
                 | CLASS ID body program
                 '''
-    # if (len(p) == 3):
-    #     p[0] = [p[1]] + p[2]
-    # else:
-    #     p[0] = [p[1]]
+    if (len(p) == 3):
+        p[0] = [p[1]] + p[2]
+    else:
+        p[0] = [p[1]]
 
 def p_funcdecl(p):
     '''funcdecl : signature body'''
-    # p[0] = sa.FuncDeclConcrete(p[1], p[2])
+    p[0] = FuncDeclConcrete(p[1], p[2])
 
 def p_signature(p):
     '''signature : ID ID LPAREN sigparams RPAREN
                  | ID ID LPAREN RPAREN'''
-    # if (isinstance(p[4], sa.SigParams)):
-    #     p[0] = sa.SignatureConcrete(p[1], p[2], p[4])
-    # else:
-    #     p[0] = sa.SignatureConcrete(p[1], p[2], None)
+    if (isinstance(p[4], SigParams)):
+        p[0] = SignatureConcrete(p[1], p[2], p[4])
+    else:
+        p[0] = SignatureConcrete(p[1], p[2], None)
 
 def p_sigparams(p):
     '''sigparams : ID ID
                   | ID ID COMMA sigparams
     '''
-    # if (len(p) == 3):
-    #     p[0] = sa.SingleSigParams(p[1], p[2])
-    # else:
-    #     p[0] = sa.CompoundSigParams(p[1], p[2], p[4])
+    if (len(p) == 3):
+        p[0] = SingleSigParams(p[1], p[2])
+    else:
+        p[0] = CompoundSigParams(p[1], p[2], p[4])
 
 def p_body(p):
     ''' body : LKEY stms RKEY
              | LKEY RKEY
              | stms'''
-    # if (len(p) == 4):
-    #     p[0] = sa.BodyConcrete(p[2])
-    # else:
-    #     p[0] = sa.BodyConcrete(None)
+    if (len(p) == 4):
+        p[0] = BodyConcrete(p[2])
+    else:
+        p[0] = BodyConcrete(None)
 
 def p_stms(p):
     ''' stms : stm
             | stm stms'''
-    # if (len(p) == 2):
-    #     p[0] = sa.SingleStm(p[1])
-    # else:
-    #     p[0] = sa.CompoundStm(p[1], p[2])
+    if (len(p) == 2):
+        p[0] = SingleStm(p[1])
+    else:
+        p[0] = CompoundStm(p[1], p[2])
 
 def p_stm(p):
     ''' stm :  exp SEMICOLON
@@ -62,14 +63,14 @@ def p_stm(p):
              | FOR LPAREN exp SEMICOLON exp SEMICOLON exp RPAREN body
              | s             
              | RETURN exp SEMICOLON'''
-    # if (len(p) == 3):
-    #     p[0] = sa.StmExp(p[1])
-    # elif (p[1] == 'while'):
-    #     p[0] = sa.StmWhile(p[3], p[5])
-    # elif (p[1] == 'return'):
-    #     p[0] = sa.StmReturn(p[2])
-    # else:
-    #     print('Gerei None', p[1])
+    if (len(p) == 3):
+        p[0] = StmExp(p[1])
+    elif (p[1] == 'while'):
+        p[0] = StmWhile(p[3], p[5])
+    elif (p[1] == 'return'):
+        p[0] = StmReturn(p[2])
+    else:
+        print('Gerei None', p[1])
 
 def p_s(p):
     ''' s :   s1 
@@ -94,36 +95,36 @@ def p_s2(p):
 def p_exp_assign(p):
     ''' exp :   exp ASSIGN exp1
               | exp1'''
-    # if (len(p) == 2):
-    #     p[0] = p[1]
-    # else:
-    #     p[0] = sa.AssignExp(p[1], p[3])
+    if (len(p) == 2):
+        p[0] = p[1]
+    else:
+        p[0] = AssignExp(p[1], p[3])
 
 def p_exp1_sum(p):
     '''exp1 : exp1 SUM exp2
          | exp2'''
-    # if len(p) == 2:
-    #     p[0] = p[1]
-    # else:
-    #     p[0] = sa.SomaExp(p[1], p[3])
+    if len(p) == 2:
+        p[0] = p[1]
+    else:
+        p[0] = SomaExp(p[1], p[3])
 
 
 def p_exp2_sub(p):
    '''exp2 : exp2 SUB exp3
            | exp3'''
-#    if len(p) == 2:
-#        p[0] = p[1]
-#    else:
-#        p[0] = sa.MulExp(p[1], p[3])
+   if len(p) == 2:
+       p[0] = p[1]
+   else:
+       p[0] = MulExp(p[1], p[3])
 
 
 def p_exp3_times(p):
     '''exp3 : exp4 TIMES exp3
             | exp4'''
-    # if len(p) == 2:
-    #     p[0] = p[1]
-    # else:
-    #     p[0] = sa.PotExp(p[1], p[3])
+    if len(p) == 2:
+        p[0] = p[1]
+    else:
+        p[0] = PotExp(p[1], p[3])
 
 def p_exp4_times(p):
     '''exp4 : exp5 DIV exp4
@@ -211,19 +212,19 @@ def p_types(p):
 def p_call_id_params(p):
     '''call : ID LPAREN params RPAREN
             | ID LPAREN RPAREN'''
-    # if len(p) == 5:
-    #     p[0] = sa.ParamsCall(p[1], p[3])
-    # else:
-    #     p[0] = sa.NoParamsCall(p[1])
+    if len(p) == 5:
+        p[0] = ParamsCall(p[1], p[3])
+    else:
+        p[0] = NoParamsCall(p[1])
 
 
 def p_params_ids(p):
     '''params : exp COMMA params
             | exp '''
-    # if len(p) == 2:
-    #     p[0] = sa.SingleParam(p[1])
-    # elif len(p) == 4:
-    #     p[0] = sa.CompoundParams(p[1], p[3])
+    if len(p) == 2:
+        p[0] = SingleParam(p[1])
+    elif len(p) == 4:
+        p[0] = CompoundParams(p[1], p[3])
 
 def p_error(p):
     print("Syntax error in input!")
